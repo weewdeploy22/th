adbd19200b8b6edd7eb3ac63c76da38200512233
weejobs <weejobs@icloud.com>
2022-09-05 10:32:04 +0700

    add libFileDB.

--- src/env/constData.js
+++
@@ -1,9 +0,0 @@
-import { ALL_TASKS } from "./all.js"
-// import { ALL_TASKS } from "./mini.js"
-
-import fetchOptions from "./fetchOptions.js"
-
-export {
-    ALL_TASKS,
-    fetchOptions,
-}
--- src/env/mini.js
+++
@@ -1,6 +0,0 @@
-const ALL_TASKS = [
-    "SCP",
-    "CHG11C2302A",
-]
-
-export { ALL_TASKS }
