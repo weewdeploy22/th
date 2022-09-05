60c6034417b342236363249335400678026686f2
weejobs <weejobs@icloud.com>
2022-09-05 16:48:45 +0700

    add bench(bench--conn with-got).

--- src/bench/bench-conn.js
+++ src/bench/bench-conn.js
@@ -3,12 +3,14 @@
 import path from 'path';
 import { fileURLToPath } from 'url'
 
+import got from 'got';
+
 const nodePath = path.resolve(process.argv[1]);
 const modulePath = path.resolve(fileURLToPath(import.meta.url))
 const isRunningDirectlyViaCLI = nodePath === modulePath
 
-const URL = 'https://wen043.settrade.com/webrealtime/data/fastquote.jsp';
-// const URL = 'https://pokeapi.co/api/v2/pokemon/1';
+// const URL = 'https://wen043.settrade.com/webrealtime/data/fastquote.jsp';
+const URL = 'https://pokeapi.co/api/v2/pokemon/1';
 
 const OPTIONS = {
 
@@ -33,20 +35,30 @@ const OPTIONS = {
 
 const oneFetch = async function (someParams) {
     let hasErr = false;
-    // for (let i = 0; i < 2; i++) {
-    for (let i = 0; i < 12; i++) {
-        const res = await fetch(URL).catch(function (err) {
-            hasErr = true;
+    for (let i = 0; i < 2; i++) {
+        // for (let i = 0; i < 12; i++) {
+        try {
+            const res = await got(URL, { "method": "GET" });
+            if (res.ok) {
+                const body = res.body;
+            } else {
+                hasErr = true;
+            }
+        } catch (error) {
+            // console.error(error)
+            console.log('--catch--')
 
-            return false;
-        });
-
-        if (res === false) {
             hasErr = true;
-        } else {
-            const hasData = await res.text();
-            // console.log(hasData);
         }
+
+        // const res = await fetch(URL).catch(function (err) {
+        // if (res === false) {
+        //     hasErr = true;
+        // } else {
+        //     // const hasData = await res.text();
+        //     const hasData = await res.body();
+        //     // console.log(hasData);
+        // }
 
     }
 
@@ -60,8 +72,10 @@ async function main() {
     let numConn = 5;
     let hasErr = false;
 
-    const timeBenchLimit = 2 * 1000; //ms
+    const timeBenchLimit = 700; //ms
+    // const timeBenchLimit = 2 * 1000; //ms
 
+    let loop_i = 1;
     while (!hasErr) {
         console.log('numConn: ', numConn);
         console.time('allConn');
@@ -78,10 +92,11 @@ async function main() {
                 console.error(err);
             });
 
+        // console.log('results: ', results);
         // check-err http
         for (let i = 0; i < results.length; i++) {
             const resultErr = results[i];
-            local_hasErr = resultErr;
+            const local_hasErr = resultErr;
             if (local_hasErr) {
                 hasErr = true;
                 console.log(`err-on: -- [numConn=${numConn}, i=${i}] -- `);
@@ -91,14 +106,17 @@ async function main() {
 
         // --time-stop
         const timeStop = Date.now();
-        const timeUsed = timeStop - timeStart; 
+        const timeUsed = timeStop - timeStart;
         // check-err long-time
         if (timeUsed > timeBenchLimit) {
             hasErr = true;
             console.log('err::: outOfTime - timeUsed: ', timeUsed);
-            console.log(`err-on: -- [numConn=${numConn}, i=${i}] -- `); 
+            console.log(`err-on: -- [numConn=${numConn}, i=${loop_i}] -- `);
             break;
         }
+
+        console.log(`log-at: -- [numConn=${numConn}, i=${loop_i}] -- `);
+        console.log('timeUsed: ', timeUsed);
 
         const sleepTimeMs = 10;
         await new Promise(r => setTimeout(r, sleepTimeMs));
@@ -107,6 +125,7 @@ async function main() {
 
         // double(x + 0.5)--numConn
         numConn = numConn + ((numConn / 2) | 0)
+        loop_i++;
     }
 }
 
